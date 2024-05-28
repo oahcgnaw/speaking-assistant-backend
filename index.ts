4f70dfa0c8f5104bc8a0c0fd5503340cf757c00d
Towski0804 <superchao0206@gmail.com>
2024-05-28 21:52:27 +0800

    initial-commit

---
+++ index.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config'
+
+const cors_headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+const server = Bun.serve({
+    hostname: "0.0.0.0",
+    port: process.env.PORT || 3000,
+    async fetch(req) {
+        const url = new URL(req.url);
+        const path = url.pathname;
+        if (req.method === 'OPTIONS') {
+            return new Response(null, {
+              headers: {
+                ...cors_headers,
+              },
+            });
+        }
+        if (req.method === 'OPTIONS') {
+            return new Response(null, {
+              headers: {
+                ...cors_headers,
+              },
+            })
+        }
+        if (path === '/api/v1/validate') {
+            const body = await req.json();
+            if (body.password === process.env.PASSWORD) {
+                return new Response(JSON.stringify({
+                    validated: true}), {
+                        headers: {
+                            ...cors_headers,
+                        },
+                    })
+            } else {
+                return new Response(JSON.stringify({
+                    validated: false}), {
+                        headers: {
+                            ...cors_headers,
+                        },
+                    })
+            }
+        } return new Response('Not found', {
+            headers: {
+                ...cors_headers,
+            },
+        })
+    },
+})
+
+console.log(`Listening on http://localhost:${server.port}`);
